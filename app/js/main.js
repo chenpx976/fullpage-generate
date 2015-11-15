afc45c8efe7a8b1c67bb12287b50a00f05f7dc3a
chenpx976 <chenpx976@gmail.com>
2015-11-15 15:34:47 +0800

    feat:page4 done

--- app/js/main.js
+++ app/js/main.js
@@ -2,5 +2,9 @@
 window.$ = window.jQuery = require('jquery');
 var fullpage = require('fullpage.js');
 $(function() {
-  $('#fullpage').fullpage();
+  $('#fullpage').fullpage({
+    verticalCentered: true,
+    anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7', 'page8', 'page9'],
+    navigation: true
+  });
 })
